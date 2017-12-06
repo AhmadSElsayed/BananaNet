3c25dc2dc43c301f84b3724d3666de3d851c294c
Ahmad S. Elsayed <ahmadelsayed@localhost.localdomain>
2017-12-06 22:18:51 +0200

    Upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='BananaNet',
+    version='5.0',
+    description='Keras + Caffee training. mxnet predict. OpenCV Camera + Draw',
+    author='Banana',
+    install_requires=['mxnet', 'opencv-python', 'numpy']
+)
